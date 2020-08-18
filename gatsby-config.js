0160d74c0fe1f4a0b2fff3296aecdfc1d7bdaf0d
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-08-18 15:35:18 -0300

    Add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/pages/vendas`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
